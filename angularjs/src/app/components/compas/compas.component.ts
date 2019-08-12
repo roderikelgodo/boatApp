d4edd76b97a88eccddf069f7accd30352c511996
Rodrigo Fernández <roderikelgodo@gmail.com>
2019-08-12 17:27:59 +0200

    Compas responsive

--- angularjs/src/app/components/compas/compas.component.ts
+++ angularjs/src/app/components/compas/compas.component.ts
@@ -5,6 +5,7 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-compas',
   templateUrl: './compas.component.html',
+  styleUrls: ['./compas.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class CompasComponent implements OnInit {
@@ -19,10 +20,10 @@ export class CompasComponent implements OnInit {
   //     this.donnes(data);
   //   }
   // }
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(){}
-  ngOnChanges(){
+  ngOnInit() { }
+  ngOnChanges() {
     this.donnes(this.data);
   }
 
@@ -35,46 +36,47 @@ export class CompasComponent implements OnInit {
 
     let spans = $('.grades').find('span');
 
-    let angle = parseInt( bateau.compas );
+    let angle = parseInt(bateau.compas);
 
     const anglesCoord = ['N  0°', '45°', 'E 90°', '135°', 'S 180°', '225°', 'O 270°', '315°'];
     const windTypes = ['Face', 'Près', 'Travers', 'Large', 'Arrière', 'Large', 'Travers', 'Pres'];
     const angles = [0, 45, 90, 135, 180, 225, 270, 315];
 
-    $(spans).each(function(i: any, span: any) {
-        const grado = (angle === 0 ? (i * 45) : (angle));
+    $(spans).each(function (i: any, span: any) {
+      const grado = (angle === 0 ? (i * 45) : (angle));
 
-        $(span).attr('grado', grado)
-        $(span).css({
-            WebkitTransform: 'rotate(' + grado + 'deg)'
-        });
+      $(span).attr('grado', grado)
+      $(span).css({
+        WebkitTransform: 'rotate(' + grado + 'deg)'
+      });
 
-        $(span).css('border', '2x dashed')
-        $(span).text(anglesCoord[i]);
-          $(span).css({
-              WebkitTransform: 'rotate(' + grado + 'deg)'
-          });
-        if (grado === 360 || grado === 0) {
-            $(span).css('color', 'red');
-        }
+      $(span).css('border', '2x dashed')
+      $(span).text(anglesCoord[i]);
+      $(span).css({
+        WebkitTransform: 'rotate(' + grado + 'deg)'
+      });
+      if (grado === 360 || grado === 0) {
+        $(span).css('color', 'red');
+      }
 
-        angle += 45;
+      angle += 45;
     });
 
     spans = $('.grades_vent').find('span');
     $('.vent').remove();
     let reached = false;
     let windType = '';
-    $(spans).each(function(i: any, span: any) {
+    $(spans).each(function (i: any, span: any) {
 
       if (vent.angle > angles[i] && vent.angle <= angles[i + 1]) {
+
         $(span).append('<div class="vent"></div>');
         $(span).css({
           WebkitTransform: 'rotate(' + angles[i + 1] + 'deg)'
         });
         reached = true;
         windType = windTypes[i + 1];
-      } else if(typeof angles[i + 1] === 'undefined' && !reached){
+      } else if (typeof angles[i + 1] === 'undefined' && !reached) {
         // $(spans[0]).append('<div class="vent"></div>');
         windType = windTypes[0];
       }
